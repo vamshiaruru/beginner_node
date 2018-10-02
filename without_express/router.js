bcd3304bb4de2b23803423cc1a6dfa438e41f1b0
vamshiaruru <vamshiaruru0@gmail.com>
2018-10-02 12:27:06 +0530

    corrected a mistake

---
+++ without_express/router.js
@@ -0,0 +1,14 @@
+
+n route(handle, pathname, response, postData){
+    console.log("Routing request for " + pathname);
+    if (typeof handle[pathname] === "function"){
+        handle[pathname](response, postData);
+    } else {
+        console.log("No request handler found for this path");
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not found");
+        response.end();
+    }
+}
+
+exports.route = route;
